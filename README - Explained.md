2b01ccbb8890991262e3febdcd090ae35215c612
gaijinusa <drbeaman@gmail.com>
2018-12-20 22:34:56 -0800

    fixed function argument, added comments; added README - Explained

---
+++ README - Explained.md
@@ -0,0 +1,13 @@
+# README - Explained
+
+This project asked us to create a web page that has two main functions:
+1. append data as a table from a JavaScript file;
+2. append only filtered 'date' data in the input box from said table.
+
+Relevant files include:
+1. static/data.js: This is the original data file used to load.  Note dates for input field must be between 1/10/2010 - 1/13/2010 because those are the parameters in this source file. 
+2. static/app.js: File where I wrote code to render Table and Filtered Table upon 'Submit' click.  Comments annotated throughout this file.
+3. static/index.html: HTML shell used to render the results of the JS code.  Used Bootstrap for website and styling framework.
+4. static/styles.css: CSS for HTML styling.
+5. Images/nasa.jpg: heading image.
+6. README.md: Homework assignment instructions.
--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,7 @@ var submit = d3.select("#filter-btn");
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
+// Append the table rows, data and values into the table body for each record
 function showTable() {
     tableData.forEach((sighting) => {
         var row = tbody.append("tr");
@@ -17,43 +18,42 @@ function showTable() {
     })
 };
 
+// Create filter functiont that only renders table data for date submitted in input field
 submit.on("click", function() {
 
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
 
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
 
-  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-  
-  console.log(inputValue);
-  
-  filteredData.forEach((sighting) => {
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
-      var cell = tbody.append("td");
-      cell.text(value);
-    });
-    });
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
 
+    // Print the date in the console log to verify date
+    console.log(inputValue);
+    
+    // Run the same append function above but only for filtered data
+    function updateTable(filteredData) {
+        // First clear any prior search data from the table
+        tbody.html('');
+        filteredData.forEach((sighting) => {
+            var row = tbody.append("tr");
+            Object.entries(sighting).forEach(([key, value]) => {
+                var cell = tbody.append("td");
+                cell.text(value);
+                });
+            });
+    }
+
+    // Print the data in the console log to verify function is running
     console.log(filteredData);
-  
+    
+    // Run the filtered date append data function
+    updateTable(filteredData);
 });
 
-// var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-
-// console.log(filteredData);
-// // // Append data into table
-// filteredData.forEach((sighting) => {
-//     var row = tbody.append("tr");
-//     Object.entries(sighting).forEach(([key, value]) => {
-//       var cell = tbody.append("td");
-//       cell.text(value);
-//     });
-//   });
-
+// Run the Append table function when web page first loads
 showTable();
